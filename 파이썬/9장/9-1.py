ac0a21fb38e312421f24953ab9c9c69917e3f887
thinghon <106126533+thinghon@users.noreply.github.com>
2025-10-04 17:19:52 +0900

    Update 9-1.py

--- 파이썬/9장/9-1.py
+++ 파이썬/9장/9-1.py
@@ -5,7 +5,7 @@ app=Tk()
 app.title("Head First Mix")
 app.geometry("250x100+200+100")
 
-sound_file=("C:\\Users\\박소호\\Documents\\프로그래밍1\\자료\\mix_sound\\50459_M_RED_Nephlimizer.wav")
+sound_file=("프로그래밍1\\자료\\mix_sound\\50459_M_RED_Nephlimizer.wav")
 
 mixer=pygame.mixer
 mixer.init()
@@ -30,3 +30,4 @@ stop_button.pack(side=RIGHT)
 app.protocol("WM_DELETE_WINDOW",shutdown)
 
 app.mainloop()
+
